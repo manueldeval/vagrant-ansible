8a13f15aa1d04ecea3f66279674ca5a1bc4ffa9a
Manuel Deval <manuel.deval@gmail.com>
2015-09-27 15:03:53 +0200

    utilisation d'un fichier json pour la conf en plus des var. d'env et arguments

--- python/testinfra.sh
+++ python/testinfra.sh
@@ -5,6 +5,12 @@ else
 	INFRA=$1
 fi
 
+if [ "x$INFRA" == "x" ]; then
+	echo "File"
+	INFRA=`cat .config | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["playbook_path"]';`
+	echo $INFRA
+fi
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 OLDDIR=`pwd`
 
--- ruby/custom_args.rb
+++ ruby/custom_args.rb
@@ -20,19 +20,47 @@ class CustomArgs
 			end
 		end
 
-		@playbook_path="infrastructures/haproxy_simple"
+		@playbook_path=""
         if @customOptions.has_key?("playbook_path")
 			@playbook_path=customOptions["playbook_path"]
 		elsif ENV.has_key?("playbook_path")
 			@playbook_path=ENV["playbook_path"]
         end
 
-		@box_type="ubuntu/vivid64"
+		@box_type=""
         if @customOptions.has_key?("box_type")
 			@box_type=customOptions["box_type"]
 		elsif ENV.has_key?("box_type")
 			@box_type=ENV["box_type"]
-        end	end
+        end
+
+        if @playbook_path == "" || @box_type == ""
+        	if File.exist?('.config')
+        		file = File.read('.config')
+				data = JSON.parse(file)
+				@playbook_path = data["playbook_path"]
+				@box_type = data["box_type"]				
+        	else
+        		puts "You must provide box_type and playbook_path :"
+        		puts " >> As an environment var"
+        		puts " $ export box_type=ubuntu/vivid64"
+				puts " $ export playbook_path=infrastructures/haproxy_simple"
+        		puts " "
+				puts " >> or as a vagrant custom-option"
+				puts " vagrant --custom-option='{\"box_type\":\"ubuntu/vivid64\",\"playbook_path\":\"infrastructures/haproxy_simple\"}' up/provision/..."
+        		puts " "				
+				puts " >> or in the .config file"
+				puts " $ cat ./.config"
+				puts " $ {\"box_type\":\"ubuntu/vivid64\",\"playbook_path\":\"infrastructures/haproxy_simple\"}"
+        		exit 
+        	end
+        end
+
+		File.open(".config", "w") do |f| 
+			f.write("{\"box_type\":\""+@box_type+"\",\"playbook_path\":\""+@playbook_path+"\"}")
+		end
+
+	end
 
 	def to_s
 		s = "=========================================\n"
